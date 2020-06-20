fe1e6e61c53d7bc33cc123dc34c78ac8beb2fc39
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-20 20:51:35 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,26 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+new_hash = {}
+#name_hash.select do |name, value|
+  #if value < name_hash.max_by {|a,b| b
+  #finds max value for comparison
+# 1,2,5,10 ; if less than 10 , then..? store value and compare again until empty?
+#new_hash[name] << value 
 
-end
+#end
+#end
+end 
+
+
+#max_order = []
+#name_hash.collect do |name, amount| 
+ #  if amount ....
+ #  max_order << amount.max > amount 
+ #   puts name 
+ 
+#end
+#end
+#value's value  
+#determine lowest value 
+
