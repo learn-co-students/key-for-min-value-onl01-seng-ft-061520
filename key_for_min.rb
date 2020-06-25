562fdcb3cb3d5fbd9a72e06e67b8ddaf07db004e
Jose Hidago <jhidalgo521@gmail.com>
2020-06-25 17:01:16 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,4 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+
 
 def key_for_min_value(name_hash)
   lowest_value = 0
@@ -13,7 +12,5 @@ def key_for_min_value(name_hash)
     end
   end
   lowest_key
-end
 
-ikea = {:chair => 25, :table => 85, :mattress => 450}
-key_for_min_value(ikea)
+end
